bb81ef299a38c3c9a20877a6bec2ebac4a9fd893
Rob Pilling <rob pilling at gmail>
2011-07-15 17:43:34 +0100

    Added sigwinch

--- trim.c
+++ trim.c
@@ -1,74 +1,106 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 
 int max = -1;
+int max_arg = 0;
+
+int gotwinch = 0;
+int term_fd = -1;
+
+int get_term_size()
+{
+#if TIOCGSIZE
+	struct ttysize ts;
+# define COLS ts.ts_cols
+# define ROWS ts.ts_lines
+# define FLAG TIOCGSIZE
+#else
+	struct winsize ts;
+# define COLS ts.ws_col
+# define ROWS ts.ws_row
+# define FLAG TIOCGWINSZ
+#endif
+	if(ioctl(term_fd, FLAG, &ts) == -1){
+		perror("ioctl");
+		return 1;
+	}
+
+	max = COLS;
+	return 0;
+}
 
 void trim(FILE *f)
 {
-	int c, count;
-	while((c = fgetc(f)) != EOF)
+	int c, count = 0;
+cont:
+	while(errno = 0, (c = fgetc(f)) != EOF){
+		if(gotwinch){
+			gotwinch = 0;
+			get_term_size(); /* ignore error */
+		}
+
 		if(c == '\n'){
 			count = 0;
 			putchar(c);
 		}else if(++count <= max){
 			if(c == '\t'){
-				count++;
+				count++; /* FIXME */
 				fputs("  ", stdout);
 			}else
 				putchar(c);
 		}
+	}
+
+	if(errno == EINTR)
+		goto cont;
+}
+
+void winch(int sig)
+{
+	(void)sig;
+	gotwinch = 1;
+	signal(SIGWINCH, winch);
 }
 
 int main(int argc, char **argv)
 {
 	int i;
 
-#if TIOCGSIZE
-	struct ttysize ts;
-# define COLS ts.ts_cols
-# define ROWS ts.ts_lines
-# define FLAG TIOCGSIZE
+	signal(SIGWINCH, winch);
 
-#elif defined(TIOCGWINSZ)
-	struct winsize ts;
-# define COLS ts.ws_col
-# define ROWS ts.ws_row
-# define FLAG TIOCGWINSZ
+	if(!isatty(STDERR_FILENO)){
+		if(!isatty(STDOUT_FILENO)){
+			if(!isatty(STDIN_FILENO)){
+				fputs("not a tty\n", stderr);
+				return 1;
+			}else
+				term_fd = STDIN_FILENO;
+		}else
+			term_fd = STDOUT_FILENO;
+	}else
+		term_fd = STDERR_FILENO;
 
-#endif
 
-
-	for(i = 1; i < argc; i++)
-		if(*argv[i] == '-'){
+	i = 1;
+	if(argc > 1 && *argv[1] == '-'){
+		if(!strcmp(argv[1], "--")){
+			i++;
+		}else{
+			max_arg = 1;
 			if(sscanf(argv[i] + 1, "%d", &max) != 1){
 				fprintf(stderr, "Usage: %s [-N] [FILES...]\n", *argv);
 				return 1;
 			}
-		}else
-			break;
-
-	if(max == -1){
-		/* check if stdin is connected to a terminal-device */
-		int fno;
-		if(!isatty(STDERR_FILENO)){
-			if(!isatty(STDOUT_FILENO)){
-				if(!isatty(STDIN_FILENO)){
-					fputs("not a tty\n", stderr);
-					return 1;
-				}else
-					fno = STDIN_FILENO;
-			}else
-				fno = STDOUT_FILENO;
-		}else
-			fno = STDERR_FILENO;
-
-		if(ioctl(fno, FLAG, &ts) == -1){
-			perror("ioctl");
-			return 1;
 		}
+	}
 
-		max = COLS;
+	if(!max_arg){
+		if(get_term_size())
+			return 1;
 	}
 
 	if(i < argc)
